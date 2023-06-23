64cdd4a096f316321db38434d50da41c3fe0f14e
Semi Invader <=>
2023-06-23 01:17:35 -0600

    WIP tokens v0.00

---
+++ src/NSH.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+contract NSH {
+    // Mint only by access and BURN for everyone
+    // Needs to have a mapping of LPs to check and add taxes to it.
+}
---
+++ src/NUSD.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+contract NUSD {
+    // Mint only by access and BURN can be turned ON or OFF but for everyone
+    // Needs to have a mapping of LPs to check and add taxes to it.
+}
