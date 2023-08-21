f45dd3e04d5ec4c5d293c6b0f35a708b209d3fb3
Plastic Digits <plasticdigits@protonmail.com>
2023-08-20 20:47:08 -0700

    Refactor POL.sol, remove unneeded interfaces from IPOL.sol

--- src/interface/IPOL.sol
+++ src/interface/IPOL.sol
@@ -4,54 +4,45 @@ pragma solidity 0.8.19;
 
 import "openzeppelin/token/ERC20/IERC20.sol";
 
-interface IERC20Mintable is IERC20 {
-    function mint(address to, uint256 amount) external;
-}
-
-interface IPOL is IERC20Mintable {
+interface IPOL {
     function addLiquidity(
         uint256 _minLiquidity,
         uint256 _maxTokens,
-        uint256 _baseAmount
+        uint256 _tokenBAmount
     ) external returns (uint256);
 
     function removeLiquidity(
         uint256 _amount,
-        uint256 _minBase,
-        uint256 _minTokens
+        uint256 _minTokenB,
+        uint256 _minTokenA
     ) external returns (uint256, uint256);
 
     function swap(
-        uint256 _baseInput,
-        uint256 _tokenInput,
-        uint256 _baseOutput,
-        uint256 _tokenOutput,
+        uint256 _tokenBInput,
+        uint256 _tokenAInput,
+        uint256 _tokenBOutput,
+        uint256 _tokenAOutput,
         uint256 _minIntout,
         address _to
     ) external returns (uint256 _output);
 
-    function getBaseToLiquidityInputPrice(
-        uint256 _baseAmount
-    )
-        external
-        view
-        returns (uint256 liquidityMinted_, uint256 tokenAmountNeeded_);
+    function getTokenBToLiquidityInputPrice(
+        uint256 _tokenBAmount
+    ) external view returns (uint256 liquidityAmount_, uint256 tokenAAmount_);
 
-    function outputTokens(
+    function outputTokenA(
         uint256 _amount,
         bool _isDesired
     ) external view returns (uint256);
 
-    function outputBase(
+    function outputTokenB(
         uint256 _amount,
         bool _isDesired
     ) external view returns (uint256);
 
-    function addLiquidityFromBase(
-        uint256 _baseAmount
-    ) external returns (uint256);
+    function addLiquidityFromTokenB(uint256 _tokenB) external returns (uint256);
 
-    function removeLiquidityToBase(
+    function removeLiquidityToTokenB(
         uint256 _liquidity,
         uint256 _tax
     ) external returns (uint256 base_);
